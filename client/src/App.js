da5e678a544d59fa0d0471abe555a3ba0aff7649
Justyna Konikiewicz <zagorskaj@wp.pl>
2024-06-15 17:48:18 +0200

    add employees redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,15 @@ import { Container } from 'react-bootstrap'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import Login from './pages/Login'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchEmployees } from './redux/employeesReducer'
 
 function App() {
+	const dispatch = useDispatch()
+
+	useEffect(() => dispatch(fetchEmployees()), [dispatch])
+  
 	return (
 		<Container>
 			<Routes>
---
+++ client/src/redux/employeesReducer.js
@@ -0,0 +1,33 @@
+import { API_URL } from '../config'
+
+const createActionName = actionName => `app/employees/${actionName}`
+const LOAD_EMPLOYEES = createActionName('LOAD_EMPLOYEES')
+
+export const loadEmployees = payload => ({ type: LOAD_EMPLOYEES, payload })
+
+export const fetchEmployees = () => {
+	return dispatch => {
+		fetch(`${API_URL}/employees`)
+			.then(res => {
+				return res.json()
+			})
+			.then(ads => {
+				dispatch(loadEmployees(ads))
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+}
+
+const employeesReducer = (statePart = [], action) => {
+	switch (action.type) {
+		case LOAD_EMPLOYEES:
+			return [...action.payload]
+
+		default:
+			return statePart
+	}
+}
+
+export default employeesReducer
