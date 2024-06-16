9b9a7cc78d869decb25a6bf3f163827c4ba8c65b
Justyna Konikiewicz <zagorskaj@wp.pl>
2024-06-16 09:48:34 +0200

    add route, controller to edit employee in database

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -17,3 +17,35 @@ exports.getAll = async (req, res) => {
 		res.status(500).json({ message: err.message })
 	}
 }
+
+exports.edit = async (req, res) => {
+	try {
+		let { fullName, subdivision, position, status, outOfOfficeBalance, peoplePartner } = req.body
+		if (peoplePartner === null) {
+			peoplePartner = undefined
+		}
+
+		let uploadData = { fullName, subdivision, position, status, outOfOfficeBalance, peoplePartner }
+		const id = req.params.id
+
+		const employee = await prisma.employee.findUnique({
+			where: {
+				id: id,
+			},
+		})
+
+		if (employee) {
+			await prisma.employee.update({
+				where: {
+					id: id,
+				},
+				data: uploadData,
+			})
+			res.send({ message: 'Employee changed' })
+		} else {
+			res.status(404).json('Employee not found')
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+}
--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const employees = require('../controllers/employees.controller')
 
 router.get('/employees', employees.getAll)
+router.put('/employees/:id', employees.edit)
 
 module.exports = router
